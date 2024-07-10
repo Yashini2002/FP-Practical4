7756f3e0f51880d8823b2bca1b760e45f0f7d23b
Yashini2002 <140165876+Yashini2002@users.noreply.github.com>
2024-07-10 20:31:10 +0530

    PatternMatching.scala

--- q2.scala
+++ q2.scala
@@ -9,17 +9,5 @@ object PatternMatching{
             case _ => println("Odd number is given")
         }
 
-        /*if(args.length > 0){
-            val num = args(0).toInt
-
-            num match{
-                case n if n<= 0 => println("Negative/ Zero is input")
-                case n if n%2 == 0 => println("Even number is given")
-                case _ => println("Odd number is given")
-            }
-        }
-        else{
-            println("Please enter the number as a command-line argument.")
-        }*/
     }
 }
